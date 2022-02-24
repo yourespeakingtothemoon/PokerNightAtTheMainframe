036690a035ee5844bf18328640d10b7add3759ba
bsmithNeumont <98478090+bsmithNeumont@users.noreply.github.com>
2022-02-24 14:06:18 -0700

    Model classes (DECK GENERATION YES WORKY)

--- src/PokerNight/Main.java
+++ src/PokerNight/Main.java
@@ -5,7 +5,6 @@ import PokerNight.Model.Deck;
 public class Main {
     public static void main(String[] args) {
         Deck deck = new Deck();
-        System.out.println("I'm gae");
         System.out.println(deck.GenerateDeck());
     }
 }
--- src/PokerNight/Model/Deck.java
+++ src/PokerNight/Model/Deck.java
@@ -5,11 +5,11 @@ import java.util.ArrayList;
 public class Deck {
     ArrayList<Card> deckList = new ArrayList<>(52);
 
-    public ArrayList<Card> GenerateDeck() {
-        for (int x = 0; x < Suit.values().length; x++) {
-            for (int y = 0; x < Rank.values().length; y++) {
-                System.out.println(Rank.values()[y] + " of " + Suit.values()[x]);
-                deckList.add(new Card(Suit.values()[x], Rank.values()[y]));
+    public ArrayList<Card> GenerateDeck() { //Generates a full deck of 52 different cards
+        for (int x = 0; x < Suit.values().length; x++) { //For each suit
+            for (int y = 0; y < Rank.values().length; y++) { //For each rank
+//                System.out.println(Rank.values()[y] + " of " + Suit.values()[x]);
+                deckList.add(new Card(Suit.values()[x], Rank.values()[y])); //Creates a card of Suit, Rank; effectively each combination
             }
         }
         return deckList;
