de7ed3b4a819ddb409d1d3dcaa75a7166ba36759
yourespeakingtothemoon <manen@tutanota.com>
2022-02-25 15:26:34 -0700

    json containing dialogue

--- src/PokerNight/DAL/DAPoker.java
+++ src/PokerNight/DAL/DAPoker.java
@@ -6,7 +6,7 @@ public class DAPoker {
     - make JSON with all dialogue and text required for characters with
     key value format being like bluff-ID-RAND
     - create a method that pulls from passed in info using the ID number and a Randomly generated number
-    IF TIME PERMITS
+    *******IF TIME PERMITS*******
     - create a persistent high score system using another JSON file.
      */
 }
