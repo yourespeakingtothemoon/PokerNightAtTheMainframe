588cb35c9b0e8dff91886816a8ad9daad5b0eec9
yourespeakingtothemoon <manen@tutanota.com>
2022-03-08 22:13:38 -0700

    THE CHECKS WORK IT SAW THE PAIR IT SAW THE STRAIGHT IT SAW THE FLUSH LETS GOOOOOOOOOO

--- src/PokerNight/Main.java
+++ src/PokerNight/Main.java
@@ -1,20 +1,47 @@
 package PokerNight;
 
+import PokerNight.Controller.Checks;
 import PokerNight.Controller.StartApp;
 import PokerNight.DAL.DAPoker;
+import PokerNight.Model.Card;
+import PokerNight.Model.Deck;
 import PokerNight.View.Dialogue;
 import org.json.simple.parser.ParseException;
 
 import java.io.IOException;
+import java.util.ArrayList;
 
 public class Main {
     public static void main(String[] args) throws IOException, ParseException, InterruptedException {
 
-       /* Dialogue.printDialogue(1,9,1,"AM");
-        Dialogue.printDialogue(1,3,1,"AM");*/
-        //Dialogue.printDialogue(3,6,4,"NORM");
+
         StartApp startApp = new StartApp();
         startApp.MainMenu();
     }
     //mrmoon reporting for duty
 }
+
+
+
+//code for testing
+/* ArrayList<Card> testFlush = new ArrayList<>();
+        Deck deck = new Deck();
+        ArrayList<Card> deck2 = new ArrayList<>();
+        deck.GenerateDeck(deck2);
+        for(int pos=0;pos<=5;pos++){
+            testFlush.add(deck2.get(pos));
+        }
+        System.out.println(testFlush);
+        ArrayList<Card> testPocket=new ArrayList<>();
+        testPocket.add(testFlush.get(0));
+        testPocket.add(testFlush.get(1));
+        testFlush.removeAll(testPocket);
+        System.out.println(testFlush);
+        System.out.println(testPocket);
+        System.out.println(Checks.probScore(5,testPocket,testFlush));
+        testFlush.remove(testFlush.get(1));
+        testFlush.add(testPocket.get(1));
+        System.out.println(Checks.probScore(5,testPocket,testFlush));*/
+ /* Dialogue.printDialogue(1,9,1,"AM");
+        Dialogue.printDialogue(1,3,1,"AM");*/
+//Dialogue.printDialogue(3,6,4,"NORM");
